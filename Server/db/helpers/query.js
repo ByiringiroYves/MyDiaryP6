48d5635aa88562570e6947e4112934359554437d
Byiringiroyves <byiringiroyves61@gmail.com>
2019-11-04 14:18:38 +0200

    ft(singup and login apis connected

--- Server/db/helpers/query.js
+++ Server/db/helpers/query.js
@@ -12,18 +12,10 @@ const CREATE_USER = `INSERT INTO
         VALUES($1, $2, $3, $4, $5, $6)
         returning *`;
 const GET_USER_BY_EMAIL = "SELECT * FROM users WHERE email = $1";
-const CREATE_ENTRY_TABLE = `CREATE TABLE IF NOT EXISTS
-      entries(
-        id UUID PRIMARY KEY,
-        title VARCHAR(128) NOT NULL,
-        description VARCHAR(1000) NOT NULL,
-        email VARCHAR(128) NOT NULL,
-        createdOn TIMESTAMP
-      )`;
+
        
 export { 
   CREATE_USER_TABLE, 
   CREATE_USER, 
-  GET_USER_BY_EMAIL,
-  CREATE_ENTRY_TABLE 
+  GET_USER_BY_EMAIL
 };
--- Server/routes/User.js
+++ Server/routes/User.js
@@ -4,7 +4,7 @@ import UserMiddleware from '../middleware/User';
 
 const router = express.Router();
 
-router.post('/api/v1/auth/signup', UserMiddleware.userNotExists, User.createUser);
-router.post('/api/v1/auth/signin', User.login);
+router.post('/api/v2/auth/signup', UserMiddleware.userNotExists, User.createUser);
+router.post('/api/v2/auth/signin', User.login);
 
 export default router;
