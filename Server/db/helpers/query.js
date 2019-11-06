d0436414b58a3479bc154e6d0922d628e2c67c4b
Byiringiroyves <byiringiroyves61@gmail.com>
2019-11-06 12:28:27 +0200

    applying feedbacks

--- Server/db/helpers/query.js
+++ Server/db/helpers/query.js
@@ -1,9 +1,11 @@
-const CREATE_USER_TABLE = `CREATE TABLE IF NOT EXISTS
+const CREATE_USER_TABLE = `
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS
     users(
       id UUID PRIMARY KEY,
       firstName VARCHAR(128) NOT NULL,
       lastNAme VARCHAR(128) NOT NULL,
-      email VARCHAR(128) UNIQUE NOT NULL,
+      email VARCHAR(128) NOT NULL,
       password VARCHAR(128) NOT NULL,
       createdOn TIMESTAMP
     )`;
@@ -12,18 +14,10 @@ const CREATE_USER = `INSERT INTO
         VALUES($1, $2, $3, $4, $5, $6)
         returning *`;
 const GET_USER_BY_EMAIL = "SELECT * FROM users WHERE email = $1";
-const CREATE_ENTRY_TABLE = `CREATE TABLE IF NOT EXISTS
-      entries(
-        id UUID PRIMARY KEY,
-        title VARCHAR(128) NOT NULL,
-        description VARCHAR(1000) NOT NULL,
-        email VARCHAR(128) NOT NULL,
-        createdOn TIMESTAMP
-      )`;
+
        
 export { 
   CREATE_USER_TABLE, 
   CREATE_USER, 
-  GET_USER_BY_EMAIL,
-  CREATE_ENTRY_TABLE 
+  GET_USER_BY_EMAIL
 };
